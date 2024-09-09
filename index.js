8752459649a2c2a79581ed6d47b62257f79305a4
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2024-09-09 17:20:28 -0300

    criei telas de login e encaminhei para o admin/usuarios/lst

--- index.js
+++ index.js
@@ -5,8 +5,37 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+import usuario from './models/usuario.js'
+
 app.get('/cadastro', (req, res)=>{
     res.render('cadastro')
+})
+
+app.post('/cadastro', async (req, res)=>{
+    //esse comando equivale a um if
+    const admin = req.body.admin=="on" ? true : false;
+
+    const novousuario = new usuario({
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha,
+        endereco: req.body.endereco,
+        foto: req.body.foto,
+        telefone: req.body.telefone,
+        cpf: req.body.cpf,
+        admin: admin
+    })
+
+    await novousuario.save();
+    res.send("Cadastrado com sucesso!")
+})
+
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
+
+app.post('/login', (req, res)=>{
+    res.redirect('/admin/usuarios/lst')
 })
 
 app.listen(port);
