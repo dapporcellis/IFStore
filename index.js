5535cd3535732613da29cbea554b07ad918b0ec8
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2024-09-17 14:37:57 -0300

    usando mvc

--- index.js
+++ index.js
@@ -5,41 +5,10 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-import usuario from './models/usuario.js'
+import publicroutes from './routes/public.js';
+import adminroutes from './routes/admin.js';
 
-app.get('/cadastro', (req, res)=>{
-    res.render('cadastro')
-})
-
-app.post('/cadastro', async (req, res)=>{
-    //esse comando equivale a um if
-    const admin = req.body.admin=="on" ? true : false;
-
-    const novousuario = new usuario({
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha,
-        endereco: req.body.endereco,
-        foto: req.body.foto,
-        telefone: req.body.telefone,
-        cpf: req.body.cpf,
-        admin: admin
-    })
-
-    await novousuario.save();
-    res.send("Cadastrado com sucesso!")
-})
-
-app.get('/login', (req, res)=>{
-    res.render('login')
-})
-
-app.post('/login', (req, res)=>{
-    res.redirect('/admin/usuarios/lst')
-})
-
-app.get("/admin/usuarios/lst", (req, res)=>{
-    res.render('admin/usuarios/lst')
-})
+app.use(publicroutes);
+app.use(adminroutes);
 
 app.listen(port);
