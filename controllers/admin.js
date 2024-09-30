52c0b9d65ee6734c524d12cae378582dd1927272
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2024-09-30 17:13:43 -0300

    Adiciona categoria

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 import Usuario from '../models/usuario.js';
+import Categoria from '../models/categoria.js';
 
 export async function listarusuarios(req, res){
     console.log("AQUI"+req.params)
@@ -9,4 +10,15 @@ export async function listarusuarios(req, res){
 export async function detalhe(req, res) {
     const usuario = await Usuario.findById(req.params.id);
     res.render('admin/usuarios/detalhe', {usuario: usuario});
+}
+
+export async function abreaddcategoria(req, res) {
+    res.render('admin/categoria/add')
+}
+
+export async function addcategoria(req, res) {
+    await Categoria.create({
+        nome:req.body.nome
+    })
+    res.redirect('/admin/categoria/add')
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,14 @@
 import express from 'express'
 const router = express.Router()
 
-import {listarusuarios, detalhe} from '../controllers/admin.js';
+import {listarusuarios, detalhe, abreaddcategoria, addcategoria} from '../controllers/admin.js';
 
 router.get("/admin/usuarios/lst", listarusuarios)
 router.get("/admin/usuarios/detalhe/:id", detalhe)
+
+//create do modelo categoria
+router.get('/admin/categoria/add', abreaddcategoria)
+router.post('/admin/categoria/add', addcategoria)
+
 
 export default router
