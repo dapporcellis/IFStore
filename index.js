7c3a616203a15fbfafdde73f29909c896a640a53
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2024-10-21 16:08:13 -0300

    Alterações de multer

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 import publicroutes from './routes/public.js';
 import adminroutes from './routes/admin.js';
